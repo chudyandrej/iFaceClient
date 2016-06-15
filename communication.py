1afda4b31f9c3ae8267616e066f434a4648a9c6e
Andrej Chudy <andrejchudy@snimac3.internal.apis.sk>
2016-06-15 12:36:14 +0200

    Add select motions trigger

---
+++ communication.py
@@ -0,0 +1,40 @@
+import json
+import base64
+import requests
+import cv2
+
+thread_counter = 0
+
+#Function for send frame to iFace server
+def send_fame_to_iFaceSERVER(frame, gray_frame):
+    global thread_counter
+    thread_counter += 1
+    #Blury detection
+    if cv2.Laplacian(gray_frame, cv2.CV_64F).var() > 150:
+    	encoded_img = frame_to_imgB64(frame)
+    	#req = requests.post (url=URL_server, data="image="+encoded_img+"&camraID=1")
+    	#print req.text
+    	print "posielam"
+    	#if espeak:
+    #		voice_synthesizer(req.text)
+    else:
+        print "Skiped. To blurry !!!"
+
+	thread_counter -= 1
+
+
+
+def frame_to_imgB64(frame):
+	img_str = cv2.imencode('.jpg', frame)[1].tostring()
+	encoded_img = base64.encodestring(img_str)
+	encoded_img = encoded_img.replace('+', '-')
+	encoded_img = encoded_img.replace('/', '_')
+	encoded_img = encoded_img.replace('.', '.')
+	return encoded_img
+
+
+def voice_synthesizer(json_message):
+	parsed_json = json.loads(json_message)
+	if parsed_json['count'] > 0:
+		command = "espeak -v sk --stdout '%s %s' | aplay" % (parsed_json['first_name'], parsed_json['last_name'])
+		os.system(command.encode('UTF-8'))
--- iFaceClient.py
+++ iFaceClient.py
@@ -1,16 +1,12 @@
+#!/usr/bin/python
+
 import cv2
-import sys
-import time
-import base64
-import requests
 import thread
 import numpy as np
-from Queue import Queue
+import sys
+import math
 from threading import Thread
-from time import sleep
-import json
-
-
+from communication import send_fame_to_iFaceSERVER, thread_counter
 
 #SETTINGS
 URL_server = "http://192.168.1.121:8080/recognise"
@@ -18,97 +14,94 @@ WINDOW_NAME = "Camera"
 FRAMES_TO_LEARN_BG = 30
 SENSITIVITY_MOTION_TRIGGER = 3000
 
-
-
 #Atributes of program (extended)
 BLUR_SIZE = 3
 NOISE_CUTOFF = 20		# Set sensitivity between new frame and sample
 
 #Global variables
-thread_counter=0
 espeak = True
+config_mode = False
+point1 = ()
+point2 = ()
+cliced = False
 
+def click_and_crop(event, x, y, flags, param):
+	# grab references to the global variables
+	global point1, point2,cliced
 
-def frame_to_imgB64(frame):
-	img_str = cv2.imencode('.jpg', frame)[1].tostring()
-	encoded_img = base64.encodestring(img_str)
-	encoded_img = encoded_img.replace('+', '-')
-	encoded_img = encoded_img.replace('/', '_')
-	encoded_img = encoded_img.replace('.', '.')
-	return encoded_img
+	if config_mode and event == cv2.EVENT_LBUTTONDOWN:
+		cliced = True
+		point1 = (x,y)
+		point2 = (x,y)
 
-def voice_synthesizer(json_message):
-	parsed_json = json.loads(json_message)
-	if parsed_json['count'] > 0:
-		command = "espeak -v sk --stdout '%s %s' | aplay" % (parsed_json['first_name'], parsed_json['last_name'])
-		os.system(command.encode('UTF-8'))
+	if config_mode and event == cv2.EVENT_MOUSEMOVE and cliced:
+		point2 = (x,y)
 
-
-#Function for send frame to iFace server
-def send_fame_to_iFaceSERVER(frame):
-	global thread_counter
-	thread_counter += 1
-	encoded_img = frame_to_imgB64(frame)
-	#req = requests.post (url=URL_server, data="image="+encoded_img+"&camraID=1")
-	#print req.text
-	print "posielam"
-	#if espeak:
-#		voice_synthesizer(req.text)
-	thread_counter -= 1
+	if config_mode and event == cv2.EVENT_LBUTTONUP:
+		cliced = False
+		point2 = (x,y)
 
 def scene_change(frame_sample, frame_now):
 	frame_delta = cv2.absdiff(frame_sample, frame_now)
 	frame_delta = cv2.threshold(frame_delta, NOISE_CUTOFF, 255, 3)[1]
 	return cv2.countNonZero(frame_delta)
 
-
-
-
-
 # Main function of program
 def main():
+	global espeak, point1, point2, config_mode
 	if "off-espeak" in sys.argv:
-		global espeak
 		espeak = False
 	#Open video capture arg -> number (camera USB) / path to video / URL
 	cam = cv2.VideoCapture(0)
+
 	#Set size of video capture
-	cam.set(3, 720)
-	cam.set(4, 576)
+	#cam.set(3, 720)
+	#cam.set(4, 576)
 	#Creat window
 	cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_AUTOSIZE)
-	# Stabilize the detector by letting the camera warm up andseeding the first frames.
+	cv2.setMouseCallback(WINDOW_NAME, click_and_crop)
 
-	frame_sample = cam.read()[1]
+	# Operations whit first frame
 	frame_sample = cam.read()[1]
 	frame_sample = cv2.cvtColor(frame_sample, cv2.COLOR_RGB2GRAY)
 	frame_sample = cv2.blur(frame_sample, (BLUR_SIZE, BLUR_SIZE))
+
 	counter_frame = 1
+
+ 	point1 = (0, 0)
+	w, h = frame_sample.shape
+	point2 = (h-1, w-1)
 
 	while True:
 		frame = cam.read()[1]
-		cv2.imshow(WINDOW_NAME, frame)
 		frame_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-		blury = cv2.Laplacian(frame_gray, cv2.CV_64F).var()
-		if blury > 150:
-			frame_blur_gray = cv2.blur(frame_gray, (BLUR_SIZE, BLUR_SIZE))
-			counter_frame += 1
+		frame_blur_gray = cv2.blur(frame_gray, (BLUR_SIZE, BLUR_SIZE))
+		show_frame = frame.copy()
 
-			if scene_change(frame_sample,frame_blur_gray) > SENSITIVITY_MOTION_TRIGGER:
-				thread.start_new_thread(send_fame_to_iFaceSERVER,(frame,))
+		if config_mode:
+			cv2.rectangle(show_frame, point1, point2, (0, 255, 0), 2)
+			cv2.putText(show_frame,'Configure mode',(int(w-(w*0.02)),50),  cv2.FONT_HERSHEY_SIMPLEX, 2,(255,0,0),2,cv2.LINE_AA)
+		cv2.imshow(WINDOW_NAME, show_frame)
 
-			if counter_frame > FRAMES_TO_LEARN_BG:
-				frame_sample = frame_blur_gray
-				counter_frame = 0
+		counter_frame += 1
 
-			print thread_counter
-		else:
-			print "Skip to blurry !!!"
+		if scene_change(frame_sample[point1[1]:point2[1], point1[0]:point2[0]],frame_blur_gray[point1[1]:point2[1], point1[0]:point2[0]]) > SENSITIVITY_MOTION_TRIGGER:
+			thread.start_new_thread(send_fame_to_iFaceSERVER,(frame,frame_gray))
+
+		if counter_frame > FRAMES_TO_LEARN_BG:
+			frame_sample = frame_blur_gray
+			counter_frame = 0
+
+		print thread_counter
+
 	    # Wait up to 1ms for a key press. Quit if the key is either ESC or 'q'.
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord("q"):
 			cv2.destroyWindow(WINDOW_NAME)
 			break
+		elif key == ord("c"):
+			config_mode = not config_mode
+
 
 if __name__ == "__main__":
     main()
